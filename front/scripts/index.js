a852b823eff73b11aef5e305c67642261d35723c
FelixIturra <felix.south.code@gmail.com>
2024-03-11 21:33:24 -0300

    actualizacion de url

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,11 +1,13 @@
-// importamos una funcion especial llamada 'crearTarjetas' desde otro archivo llamado 'card.js'.
+
+
+// importamos una función especial llamada 'crearTarjetas' desde otro archivo llamado 'card.js'.
 import { crearTarjetas } from "./card.js";
 
-// usamos 'axios' para hacer pedidos de datos desde otra parte de Internet.
+// usamos una herramienta que se llama 'axios' para hacer pedidos de datos desde otra parte de Internet.
 const axios = require("axios");
 
-// aqui estamos buscando en el código HTML los lugares donde queremos mostrar las tarjetas de pelis.
-// uno es para las pelis recien estrenadas, otro para las recomendadas y otro para todas las demás.
+// aquí estamos buscando en el código HTML los lugares donde queremos mostrar las tarjetas de pelis.
+// uno es para las pelis recién estrenadas, otro para las recomendadas y otro para todas las demás.
 const contenedorEstrenos = document.getElementById("estrenos-container");
 const contenedorRecomendados = document.getElementById(
   "recomendados-container"
@@ -14,20 +16,20 @@ const contenedorTarjetas = document.getElementById("tarjetas-container");
 
 // ahora vamos a pedirle a Internet info sobre pelis.
 axios
-  .get("https://students-api.2.us-1.fl0.io/movies")
-  // cuando obtengamos la info con exito, vamos a hacer algunas cosas con esos datos.
+  .get("https://students-api.up.railway.app/movies")
+  // cuando obtengamos la info con éxito, vamos a hacer algunas cosas con esos datos.
   .then((response) => {
     // guardamos los datos de las pelis en una variable llamada 'data'.
     const data = response.data;
 
-    // luego, usamos esos datos para crear tarjetas de pelis y mostrarlas en diferentes lugares de la pagina.
+    // luego, usamos esos datos para crear tarjetas de pelis y mostrarlas en diferentes lugares de la página.
     // primero, creamos tarjetas para pelis recién estrenadas.
     crearTarjetas(contenedorEstrenos, data);
 
-    //creamos tarjetas para pelis recomendadas.
+    // luego, creamos tarjetas para pelis recomendadas.
     crearTarjetas(contenedorRecomendados, data);
 
-    // creamos tarjetas para todas las demás pelis.
+    // y finalmente, creamos tarjetas para todas las demás pelis.
     crearTarjetas(contenedorTarjetas, data);
   })
   // si algo sale mal mientras obtenemos los datos, mostramos un mensaje de error en la consola.
